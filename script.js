e381294f56131952d4f689157fc1a25aca70972e
ぺんぎん@TRANs <137734383+pen-tech@users.noreply.github.com>
2024-05-19 12:46:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function selectGame(){
   var game = document.フォーム名.フィールド名.options[document.フォーム名.フィールド名.selectedIndex].value
 }
+
+function valueChange(){
+  console.log('新しく ' + fruitSelect.value + ' を選択しました');
+}
+
+let gameSelect = document.getElementById('game');
+gameSelect.options[2].selected = true;
+gameSelect.addEventListener('change', valueChange);
